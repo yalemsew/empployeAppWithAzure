83c5c36655f31a776259bbbf1f8aebb0dd7578ab
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-04-17 11:49:00 -0500

    test for data integration exception

--- src/test/java/com/bright/emstesting/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/bright/emstesting/repository/EmployeeRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.bright.emstesting.repository;
 
+import com.bright.emstesting.exception.employee.DuplicateEmailException;
 import com.bright.emstesting.model.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 
@@ -23,7 +25,7 @@ class EmployeeRepositoryTest {
         employee = Employee.builder()
                 .firstName("Jhon")
                 .lastName("Doe")
-                .email("<EMAIL")
+                .email("jhon.smith@gmail.coom")
                 .departmentCode("CS489")
                 .build();
     }
@@ -37,7 +39,25 @@ class EmployeeRepositoryTest {
         //Then
         Optional<Employee> employeeOptional = employeeRepository.findById(employee.getId());
         assertTrue(employeeOptional.isPresent());
-        assertEquals(employee, employeeOptional.get());
+        assertEquals("Jhon", employeeOptional.get().getFirstName());
+        assertEquals("Doe", employeeOptional.get().getLastName());
 
+    }
+    @Test
+    @DisplayName("Test for saving an existing employee")
+public void givenExistingEmployee_whenSave_thenThrowsException(){
+        //Given
+        Employee savedEmployee = employeeRepository.saveAndFlush(employee);
+        Employee employee2 = Employee.builder()
+                .firstName("Jhon")
+                .lastName("Smith")
+                .email("jhon.smith@gmail.coom")
+                .build();
+
+        //throw exception when save existing emoloyee
+        assertThrows(DataIntegrityViolationException.class, () -> employeeRepository.saveAndFlush(employee2));
+
+
+        //
     }
 }
